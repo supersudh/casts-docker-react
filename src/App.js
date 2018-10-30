1c6efe13aeee2f39bb417586154d9261a49ebdf5
Sudharsan Ravikumar <supersudh@gmail.com>
2018-10-30 13:30:55 +0530

    change text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const Comp1 = () => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     <p>
-      Edit wat<code>src/App.js</code> and save to reload.
-          </p>
+      feature branch!!<code>src/App.js</code> and save to reload.
+    </p>
     <a
       className="App-link"
       href="https://reactjs.org"
